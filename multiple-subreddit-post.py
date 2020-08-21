61d6f7c250f443defe8a80fc611fdecc6fc49eae
younggianniguy <younggianniguy@gmail.com>
2020-08-21 15:11:30 -0500

    can now post to the multiple subreddits that populate subredditList.txt and set basic options

--- multiple-subreddit-post.py
+++ multiple-subreddit-post.py
@@ -8,6 +8,7 @@ def reddit_authentication(config):
     redditClientSecret = config.get('credentials', 'reddit_Client_Secret')
     redditUsername = config.get('credentials', 'reddit_Username')
     redditPassword = config.get('credentials', 'reddit_Password')
+
     return praw.Reddit(client_id = redditClientId,
                        client_secret = redditClientSecret,
                        username = redditUsername,
@@ -27,49 +28,42 @@ class post_options:
         self.url = url
         self.flair = flair 
 
-
-    def getSubreddits():
-        return self.subreddits
-
-
-
+def select_subreddits:
+    #function to select subreddits
 
 def create_post():
-    title = input('Enter Title ')
-    #subreddit
-    url = input('Enter url (leave blank if using path) ')
-    path = input('Enter Path (leave blank if using url) ')
+    title = input('Enter Title: ')
+    url = input('Enter url (leave blank if using path): ')
+    path = input('Enter Path (leave blank if using url): ')
     flair = ''
 
-    inboxReplies = input('Disable Inbox Replies? (y/n) ')
+    inboxReplies = input('Disable Inbox Replies? (y/n): ')
     if inboxReplies == 'y':
         inboxReplies = False
     else:
         inboxReplies = True
 
-    spoiler = input('Enable Spoiler? (y/n) ')
+    spoiler = input('Enable Spoiler? (y/n): ')
     if spoiler == 'y':
         spoiler = True
     else:
         spoiler = False
 
-    originalContent = input('Mark as OC? (y/n) ')
+    originalContent = input('Mark as OC? (y/n): ')
     if originalContent == 'y':
         originalContent = True
     else:
         originalContent = False
 
-    NSFW = input('Mark as NSFW? (y/n) ')
+    NSFW = input('Mark as NSFW? (y/n): ')
     if NSFW == 'y':
         NSFW = True
     else:
         NSFW = False
     
     subreddits = [line.strip() for line in open("subredditList.txt", 'r')]
-    print(subreddits[1])
 
-    print('creating post')
-    return post_options(title, subreddits, path, url, flair, inboxReplies, spoiler, originalContent, NSFW)
+    return post_options(title, subreddits, url, path, flair, inboxReplies, spoiler, originalContent, NSFW)
 
 
 
@@ -79,11 +73,24 @@ def multiple_subreddit_post():
     config = configparser.ConfigParser()
 
     redditClient = reddit_authentication(config)
-
+    data = {'spoiler': 'True'}
     postOptions = create_post()
+    print(postOptions.url)
     for subreddit in postOptions.subreddits:
         subToPostTo = redditClient.subreddit(subreddit)
-        subToPostTo.submit(title = postOptions.title, spoiler = postOptions.spoiler, is_original_content = postOptions.originalContent, over_18 = postOptions.NSFW)
+        submission = subToPostTo.submit(title = postOptions.title, url = postOptions.url)
 
+        if not postOptions.inboxReplies:
+            submission.disable_inbox_replies()
 
+        if postOptions.spoiler:
+            submission.mod.spoiler()
+
+        if postOptions.NSFW:
+            submission.mod.nsfw()
+        else:
+            submission.mod.sfw()
+
+        if postOptions.originalContent:
+            submission.mod.set_original_content()
 multiple_subreddit_post()
